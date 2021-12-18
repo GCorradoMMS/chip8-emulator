7a0994d663370b77f65a009a471ad908b2a8b346
Gustavo Corrado <gustavocorrado98@gmail.com>
2021-12-18 00:35:42 -0300

    [ADD] - First commit

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+mod chip8; //######### CPU CORE
+
+fn main() {
+  // Set up render system and register input callbacks
+//   setupGraphics();
+//   setupInput();
+ 
+  // Initialize the Chip8 system and load the game into the memory  
+//   myChip8.initialize();
+//   myChip8.loadGame("pong"); -> Take with args
+ 
+  // Emulation loop
+//   for(;;) -> LOOP
+//   {
+//     // Emulate one cycle
+//     myChip8.emulateCycle();
+ 
+//     // If the draw flag is set, update the screen
+//     if(myChip8.drawFlag)
+//       drawGraphics();
+ 
+//     // Store key press state (Press and Release)
+//     myChip8.setKeys();	
+//   }
+
+
+// 0x00E0 – Clears the screen
+// 0xDXYN – Draws a sprite on the screen
+}

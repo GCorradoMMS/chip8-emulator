d92fb520c650c7f8b8fa9d7f4de03a7281a1eadf
Gustavo Corrado <gustavocorrado98@gmail.com>
2022-04-05 01:12:24 -0300

    [ADD] - Experimenting with graphics and new aproach to the cpu implementation

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 use std::fs::File;
 use std::io::Read;
 mod chip8; //######### CPU CORE
+mod graphics;
+mod input;
 
 fn main() {
   
   // let mut file=File::open("Cargo.toml").unwrap(); experimenting with binary reading
   // let mut buf=[0u8;12];
   // file.read(&mut buf).unwrap();
-
+  // graphics::initialize_graphics();
   chip8::initialize();
   // Set up render system and register input callbacks
 //   setupGraphics();
